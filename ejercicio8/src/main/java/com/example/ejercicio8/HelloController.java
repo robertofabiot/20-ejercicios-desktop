f73ce9a003a041d578335a5e2ef95e9166a4b529
robertofabiot <rftercerom@uamv.edu.ni>
2026-08-24 22:15:15 -0600

    feat(ejercicio 8): fully functional

--- ejercicio8/src/main/java/com/example/ejercicio8/HelloController.java
+++ ejercicio8/src/main/java/com/example/ejercicio8/HelloController.java
@@ -5,9 +5,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HelloController {
+
+    List<String> results = new ArrayList<>();
+
     @FXML
     public TextField tfNumero1;
     @FXML
@@ -18,18 +23,45 @@ public class HelloController {
     private Label welcomeText;
 
     // Esto grita PROGRAMACIÓN FUNCIONAL, pero no me sé la sintaxis y
-    // soy demasiado orgulloso para usar ia. Asi se va
+    // soy demasiado orgulloso para usar ia, así que se van sin DRY
     public void sum(ActionEvent actionEvent) {
-        Double number1, number2 = getNumbers();
+        Double number1 = Double.parseDouble(tfNumero1.getText());
+        Double number2 = Double.parseDouble(tfNumero2.getText());
+        Double result = number1 + number2;
+        results.add(number1 + " + " + number2);
+        lblResult.setText(String.valueOf(result));
+        lblHistory.setText(results.toString());
     }
 
     public void substract(ActionEvent actionEvent) {
+        Double number1 = Double.parseDouble(tfNumero1.getText());
+        Double number2 = Double.parseDouble(tfNumero2.getText());
+        Double result = number1 - number2;
+        results.add(number1 + " - " + number2);
+        lblResult.setText(String.valueOf(result));
+        lblHistory.setText(results.toString());
     }
 
     public void mutiply(ActionEvent actionEvent) {
+        Double number1 = Double.parseDouble(tfNumero1.getText());
+        Double number2 = Double.parseDouble(tfNumero2.getText());
+        Double result = number1 * number2;
+        results.add(number1 + " * " + number2);
+        lblResult.setText(String.valueOf(result));
+        lblHistory.setText(results.toString());
     }
 
     public void divide(ActionEvent actionEvent) {
+        Double number1 = Double.parseDouble(tfNumero1.getText());
+        Double number2 = Double.parseDouble(tfNumero2.getText());
+        if(number2.equals(0.0)){
+            lblResult.setText("Es en serio?");
+            return;
+        }
+        Double result = number1 / number2;
+        results.add(number1 + " / " + number2);
+        lblResult.setText(String.valueOf(result));
+        lblHistory.setText(results.toString());
     }
-    
+
 }
--- ejercicio8/src/main/java/module-info.java
+++ ejercicio8/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ejercicio8 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.ejercicio8 to javafx.fxml;
